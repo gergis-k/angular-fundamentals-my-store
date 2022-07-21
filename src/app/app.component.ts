bf94eb65bbab12310fbc5eb9a060b226f9070719
Gergis Adel <66651216+gergis-k@users.noreply.github.com>
2022-07-21 05:39:53 +0200

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'nd-0067-c3-angular-fundamentals-project-starter';
+}
